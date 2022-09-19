6513f4ba5070632b7a610437ccafd5a4009f2203
Ihor Bondar <zim20093@gmail.com>
2022-09-19 12:12:22 +0300

    initial commit

---
+++ src/main/java/clear/solutions/userapp/UserAppApplication.java
@@ -0,0 +1,13 @@
+package clear.solutions.userapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/clear/solutions/userapp/UserAppApplicationTests.java
@@ -0,0 +1,13 @@
+package clear.solutions.userapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
